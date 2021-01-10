4fd78bbcb23d43524eda8864aeece1b11e384bbc
bacloud14 <bacloud14@gmail.com>
2021-01-10 11:32:49 +0100

    wip, pattern for cache & db & routes

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var router = express.Router();
+
+var schedule = require('node-schedule');
 var uuid = require('node-uuid');
 var sqlite3 = require('sqlite3').verbose();
+const NodeCache = require( "node-cache" );
+const myCache = new NodeCache();
 var db = new sqlite3.Database(':memory:');
-
-/* GET home page. */
-router.get('/', function (req, res, next) {
-
+var rule = new schedule.RecurrenceRule();
+rule.minute = 0;
+rule.hour = 0;
+success = myCache.set( "latest",  uuid.v4() );
+var j = schedule.scheduleJob(rule, function(){
   const latest = uuid.v4();
-  const lastes_ = { latest: latest };
   console.log(latest);
   db.serialize(function () {
     db.run("CREATE TABLE IF NOT EXISTS URL (_id TEXT primary KEY, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, url TEXT)");
@@ -18,8 +22,8 @@ router.get('/', function (req, res, next) {
       }
       // get the last insert id
       console.log(`A row has been inserted with rowid ${this.lastID}`);
+      success = myCache.set( "latest", latest );
     });
-    
     db.all(`SELECT _id, timestamp, url FROM URL ORDER BY _id`, [], (err, rows) => {
       if (err) {
         console.log(err.message);
@@ -30,13 +34,39 @@ router.get('/', function (req, res, next) {
       });
     });
   });
-  
+  db.close();
+});
 
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+
+  if(myCache.has( "latest" )){
+
+  }
   res.render('index', {
     title: 'Secluded',
     message: 'Secluded is a webpage that tries to be isolated from web crawers although publically visible. It is a crawler behaviour experiment. Robots meta directives and robots.txt are pieces of code that provide crawlers instructions for how to crawl or index web page content.',
     technique: 'One hidden page is hosted. Its URL is unique and random so that no one can. It also changes over time so that we track evolution of indexing over time. Its link is withing this page so that crawlers can follow, but hopefully other websites cannot directly link to. (although they technically can just like crawlers).'
   });
+});
+
+/* GET home page. */
+router.get('/latest', function (req, res, next) {
+  console.log("latest found"+ myCache.get( "latest" ))
+  myCache.get( "latest" );
+  res.send("yes");
+});
+
+/* GET home page. */
+router.get('/earlier/:id', function (req, res, next) {
+  if(myCache.has( "latest" )){
+    console.log("wow it is found again "+ req.params.id)
+    res.send("yes it is the latest, enjoy")
+  }else{
+    // db lookup
+  }
+    
 });
 
 module.exports = router;
