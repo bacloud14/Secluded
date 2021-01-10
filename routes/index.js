b853d8077f31a076a3be77341e363084ad8faf1c
bacloud14 <bacloud14@gmail.com>
2021-01-10 21:40:39 +0100

    wip, refine error responses

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var uuid = require('node-uuid');
 var sqlite3 = require('sqlite3').verbose();
 const NodeCache = require("node-cache");
 const myCache = new NodeCache();
-var db = new sqlite3.Database('./db/secluded.db');
+var db = new sqlite3.Database('./db/secluded0.db');
 var rule = new schedule.RecurrenceRule();
 rule.minute = 0;
 rule.hour = 0;
@@ -22,10 +22,10 @@ const lorem = new LoremIpsum({
     min: 4
   }
 });
-var j = schedule.scheduleJob(rule, function () {
-  const latest = { url: uuid.v4(), content: content };
-  console.log(latest.url);
+var j = schedule.scheduleJob("*/5 * * * *", function () {
   var content = lorem.generateParagraphs(1);
+  const latest = { url: uuid.v4(), content: content };
+  console.log('\x1b[36m%s\x1b[0m', 'CRON job caching', latest.url);
   db.serialize(function () {
     db.run("CREATE TABLE IF NOT EXISTS URL (_id TEXT primary KEY, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, url TEXT, content TEXT)");
     db.run("INSERT INTO URL(_id, url, content) VALUES (?,?,?)", [latest.url, latest.url, latest.content], function (err) {
@@ -33,18 +33,18 @@ var j = schedule.scheduleJob(rule, function () {
         return console.log(err.message);
       }
       // get the last insert id
-      console.log(`A row has been inserted with rowid ${this.lastID}`);
+      console.log('\x1b[36m%s\x1b[0m', `A row has been inserted with rowid ${this.lastID}`);
       success = myCache.set("latest", latest);
     });
-    db.all(`SELECT _id, timestamp, url FROM URL ORDER BY _id`, [], (err, rows) => {
-      if (err) {
-        console.log(err.message);
-        throw err;
-      }
-      rows.forEach((row) => {
-        console.log(row);
-      });
-    });
+    // db.all(`SELECT _id, timestamp, url FROM URL ORDER BY _id`, [], (err, rows) => {
+    //   if (err) {
+    //     console.log(err.message);
+    //     throw err;
+    //   }
+    //   rows.forEach((row) => {
+    //     console.log(row);
+    //   });
+    // });
   });
 });
 
@@ -56,8 +56,8 @@ router.get('/', function (req, res, next) {
   }
   res.render('index', {
     title: 'Secluded',
-    message: 'Secluded is a webpage that tries to be isolated from web crawers although publically visible. It is a crawler behaviour experiment. Robots meta directives and robots.txt are pieces of code that provide crawlers instructions for how to crawl or index web page content.',
-    technique: 'One hidden page is hosted. Its URL is unique and random so that no one can. It also changes over time so that we track evolution of indexing over time. Its link is withing this page so that crawlers can follow, but hopefully other websites cannot directly link to. (although they technically can just like crawlers).'
+    message: 'Secluded is a webpage that tries to be isolated from web crawlers although publically visible. It is a crawler behaviour experiment. It is hopefully SEO friendly in all aspects except that it tells crawlers not to index itself. So linking to this domain to gain popularity is appreciated and thanked for.',
+    technique: 'Robots meta directives and robots.txt are pieces of code that provide crawlers instructions for how to crawl or index web page content. One hidden page is hosted. Its URL (and content) is unique and random. The latest page changes over time so that we track evolution of indexing with pages aging. Link are withing this page so that crawlers can follow.'
   });
 });
 
@@ -65,39 +65,48 @@ router.get('/', function (req, res, next) {
 router.get('/latest', function (req, res, next) {
   var url = myCache.get("latest").url
   var content = myCache.get("latest").content;
-  console.log("latest found: " + url)
+  console.log('\x1b[33m%s\x1b[0m', "Latest found ==>  " + url)
   res.send(content);
 });
 
 /* GET home page. */
 router.get('/earlier/:id', function (req, res, next) {
   if (myCache.has("latest") && req.params.id == myCache.get("latest").url) {
-    console.log("wow it is found again " + req.params.id)
-    var url = myCache.get("latest").url;
+    console.log("Found again ==>  " + req.params.id);
     var content = myCache.get("latest").content;
     res.send(content);
   } else {
-    let db = new sqlite3.Database('./db/secluded.db', (err) => {
+    let db = new sqlite3.Database('./db/secluded0.db', (err) => {
       if (err) {
         return console.error(err.message);
       }
-      let sql = `SELECT _id, timestamp, url FROM URL WHERE _id = ?`;
+      let sql = `SELECT _id, timestamp, url, content FROM URL WHERE _id = ?`;
       let url = req.params.id;
       db.get(sql, [url], (err, row) => {
         if (err) {
           return console.error(err.message);
         }
-        row ? console.log(row.id, row.timestamp, row.url) : console.log(`No URL found with the id ${url}`);
-
+        if (row) {
+          res.render('earlier', {
+            title: 'Secluded',
+            content: row.content
+          });
+        } else {
+          console.log('\x1b[31m', `No URL found with the id: ${url}`);
+          res.status(404).render('404', {
+            title: 'Secluded',
+            error_message: `No URL found with the id: ${url}`
+          });
+        }
       });
-      console.log('Connected to the in-memory SQlite database.');
     });
   }
 });
 
 process.on('SIGINT', () => {
+  j.cancel();
   db.close();
-  server.close();
+  // server.close();
 });
 
 module.exports = router;
