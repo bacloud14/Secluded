42abc780f40359bbd8c1dcd64bc2b26cb569ffd5
bacloud14 <bacloud14@gmail.com>
2021-01-11 10:37:34 +0100

    style, add sitemap

--- routes/index.js
+++ routes/index.js
@@ -22,7 +22,7 @@ const lorem = new LoremIpsum({
     min: 4
   }
 });
-var j = schedule.scheduleJob("*/5 * * * *", function () {
+var j = schedule.scheduleJob("*/1 * * * *", function () {
   var content = lorem.generateParagraphs(1);
   const latest = { url: uuid.v4(), content: content };
   console.log('\x1b[36m%s\x1b[0m', 'CRON job caching', latest.url);
@@ -36,28 +36,22 @@ var j = schedule.scheduleJob("*/5 * * * *", function () {
       console.log('\x1b[36m%s\x1b[0m', `A row has been inserted with rowid ${this.lastID}`);
       success = myCache.set("latest", latest);
     });
-    // db.all(`SELECT _id, timestamp, url FROM URL ORDER BY _id`, [], (err, rows) => {
-    //   if (err) {
-    //     console.log(err.message);
-    //     throw err;
-    //   }
-    //   rows.forEach((row) => {
-    //     console.log(row);
-    //   });
-    // });
   });
 });
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-
-  if (myCache.has("latest")) {
-
-  }
-  res.render('index', {
-    title: 'Secluded',
-    message: 'Secluded is a webpage that tries to be isolated from web crawlers although publically visible. It is a crawler behaviour experiment. It is hopefully SEO friendly in all aspects except that it tells crawlers not to index itself. So linking to this domain to gain popularity is appreciated and thanked for.',
-    technique: 'Robots meta directives and robots.txt are pieces of code that provide crawlers instructions for how to crawl or index web page content. One hidden page is hosted. Its URL (and content) is unique and random. The latest page changes over time so that we track evolution of indexing with pages aging. Link are withing this page so that crawlers can follow.'
+  db.all(`SELECT _id, timestamp, url, content FROM URL ORDER BY date(_id)`, [], (err, rows) => {
+    if (err) {
+      console.log(err.message);
+      throw err;
+    }
+    res.render('index', {
+      title: 'Secluded',
+      message: 'Secluded is a webpage that tries to be isolated from web crawlers although publically visible. It is a crawler behaviour experiment. It is hopefully SEO friendly in all aspects except that it tells crawlers not to index itself. So linking to this domain to gain popularity is appreciated and thanked for.',
+      technique: 'Robots meta directives and robots.txt are pieces of code that provide crawlers instructions for how to crawl or index web page content. One hidden page is hosted. Its URL (and content) is unique and random. The latest page changes over time so that we track evolution of indexing with pages aging. Link are withing this page so that crawlers can follow.',
+      urls_list: rows
+    });
   });
 });
 
@@ -66,7 +60,10 @@ router.get('/latest', function (req, res, next) {
   var url = myCache.get("latest").url
   var content = myCache.get("latest").content;
   console.log('\x1b[33m%s\x1b[0m', "Latest found ==>  " + url)
-  res.send(content);
+  res.render('earlier', {
+    title: 'Secluded',
+    content: content
+  });
 });
 
 /* GET home page. */
@@ -102,6 +99,48 @@ router.get('/earlier/:id', function (req, res, next) {
     });
   }
 });
+
+const { SitemapStream, streamToPromise } = require('sitemap')
+const { createGzip } = require('zlib')
+const { Readable } = require('stream')
+
+let sitemap
+
+router.get('/sitemap.xml', function(req, res) {
+  res.header('Content-Type', 'application/xml');
+  res.header('Content-Encoding', 'gzip');
+  // if we have a cached entry send it
+  if (sitemap) {
+    res.send(sitemap)
+    return
+  }
+
+  try {
+    const smStream = new SitemapStream({ hostname: 'https://example.com/' })
+    const pipeline = smStream.pipe(createGzip())
+
+    // pipe your entries or directly write them.
+    smStream.write({ url: '/page-1/',  changefreq: 'daily', priority: 0.3 })
+    smStream.write({ url: '/page-2/',  changefreq: 'monthly',  priority: 0.7 })
+    smStream.write({ url: '/page-3/'})    // changefreq: 'weekly',  priority: 0.5
+    smStream.write({ url: '/page-4/',   img: "http://urlTest.com" })
+    /* or use
+    Readable.from([{url: '/page-1'}...]).pipe(smStream)
+    if you are looking to avoid writing your own loop.
+    */
+
+    // cache the response
+    streamToPromise(pipeline).then(sm => sitemap = sm)
+    // make sure to attach a write stream such as streamToPromise before ending
+    smStream.end()
+    // stream write the response
+    pipeline.pipe(res).on('error', (e) => {throw e})
+  } catch (e) {
+    console.error(e)
+    res.status(500).end()
+  }
+})
+
 
 process.on('SIGINT', () => {
   j.cancel();
